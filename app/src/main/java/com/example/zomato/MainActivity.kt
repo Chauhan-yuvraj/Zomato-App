02496c4b3a59bd51884183fba1eda78aa3278d48
Chauhan-yuvraj <yuvibannna@gmail.com>
2024-01-27 14:04:58 +0530

    making the Sign up page

---
+++ app/src/main/java/com/example/zomato/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.zomato
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+import androidx.lifecycle.enableSavedStateHandles
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
